3f5d421bfe1c4c387666ff5a79cbc8630a5432bc
JaneKiseleva <pro.kiseleva@gmail.com>
2021-04-14 05:13:26 +0300

    version 3

--- app/src/main/java/com/example/starsgame/MainActivity.java
+++ app/src/main/java/com/example/starsgame/MainActivity.java
@@ -26,16 +26,11 @@ import java.util.regex.Pattern;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageViewStar;
-    //Создаем переменную для ссылки
-    // footbal private String url = "https://www.transfermarkt.ru/spieler-statistik/wertvollstespieler/marktwertetop";
     private String url = "https://www.forbes.ru/rating/403469-40-samyh-uspeshnyh-zvezd-rossii-do-40-let-reyting-forbes";
-    //Два массива (фото и имена)
     private ArrayList<String> urls;
     private ArrayList<String> names;
-    //Добавим все кнопки в один массив (после создания метода generateQuestion, где получаем номер вопроса рандомно)
     private ArrayList<Button> buttons;
     private ArrayList<Integer> nameIndex;
-    //Две переменные для метода generateQuestion
     private int numberOfImage;
     private int numberOfRightButton;
 
@@ -44,12 +39,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageViewStar = findViewById(R.id.imageViewStar);
-        //определяем переменным два новых массива.
         urls = new ArrayList<>();
         names = new ArrayList<>();
         buttons = new ArrayList<>();
         nameIndex = new ArrayList<>();
-        //Добавим все наши кнопки в массив
         buttons.add(findViewById(R.id.button0));
         buttons.add(findViewById(R.id.button1));
         buttons.add(findViewById(R.id.button2));
@@ -64,25 +57,14 @@ public class MainActivity extends AppCompatActivity {
         DownloadContentTask task = new DownloadContentTask();
         try {
             String content = task.execute(url).get();
-            // footbal String start = "<table class=\"items\">";
-            // footbal String finish = "<div class=\"pager\">";
             String start = "<div class=\"items\">";
             String finish = "<div class=\"panel-pane pane-rating-content\">";
             Pattern pattern = Pattern.compile(start + "(.*?)" + finish);
             Matcher matcher = pattern.matcher(content);
-            //создадим переменную с обрезанным контентом
             String splitContent = "";
             while (matcher.find()) {
                 splitContent = matcher.group(1);
             }
-
-            //Все работает, указанный нами контент вынимается в виде одной длинной строки, теперь по очередно вынимаем данные в виде массива фото и массива имен.
-            //Для этого нам понадобятся два паттерна
-
-            // footballImg  Pattern patternImg = Pattern.compile("#\"><img src=\"(.*?)\" title=");
-            // footballCoast  Pattern patternName = Pattern.compile("<span title=\"(.*?)\"");
-            // footballName Pattern patternName = Pattern.compile("<a title=\"(.*?)\" class");
-
             Pattern patternImg = Pattern.compile("<img src=\"(.*?)\"");
             Pattern patternName = Pattern.compile("alt=\"(.*?)\"/>");
             Matcher matcherImg = patternImg.matcher(splitContent);
@@ -93,20 +75,14 @@ public class MainActivity extends AppCompatActivity {
             while (matcherName.find()) {
                 names.add(matcherName.group(1));
             }
-            //проверка как все выводится
-            //for (String s: names) {
-            //Log.i("MyResult", s);
-            //}
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
     }
 
     public void playGame() {
-        //вызвали метод генерации вопросов
         numberOfRightButton = (int) (Math.random() * buttons.size());
         numberOfImage = (int) (Math.random() * names.size());
         DownloadImageTask task = new DownloadImageTask();
@@ -115,25 +91,20 @@ public class MainActivity extends AppCompatActivity {
             Bitmap bitmap = task.execute(urls.get(numberOfImage)).get();
             if (bitmap != null) {
                 imageViewStar.setImageBitmap(bitmap);
-
                 for (int i = 0; i < buttons.size(); i++) {
                     int answer;
-
                     if (i == numberOfRightButton) {
                         answer = numberOfImage;
                     } else {
                         do {
                             answer = (int) (Math.random() * names.size());
-                            //он не должен быть верным
                             if (answer == numberOfImage) continue;
-                            //он не должен совпадать с предыдущим значение
                             int flag = 0;
                             for (int j = 0; j < i; j++) {
                                 if (answer == nameIndex.get(j)) {
                                     flag = 1;
                                 }
                             }
-                            //если мы не попали на совпадение, то тогда выходим из цикла
                             if (flag == 0) break;
                         } while (true);
                     }
@@ -147,7 +118,6 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-
 
     public void onClickAnswer(View view) {
         Button button = (Button) view;
@@ -167,7 +137,6 @@ public class MainActivity extends AppCompatActivity {
             StringBuilder result = new StringBuilder();
             URL url = null;
             HttpURLConnection urlConnection = null;
-
             try {
                 url = new URL(strings[0]);
                 urlConnection = (HttpURLConnection) url.openConnection();
@@ -199,7 +168,6 @@ public class MainActivity extends AppCompatActivity {
         protected Bitmap doInBackground(String... strings) {
             URL url = null;
             HttpURLConnection urlConnection = null;
-
             try {
                 url = new URL(strings[0]);
                 urlConnection = (HttpURLConnection) url.openConnection();
